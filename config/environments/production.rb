5a853b7cf6ce4f753bbee3518ffc351e52c10035
Malaber <32635600+Malaber@users.noreply.github.com>
2019-09-14 17:37:59 +0200

    Configure right domains for different systems

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,10 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  config.hosts << 'condolence-pascal.malaber.de'
+  config.hosts << 'pascal-brand.de'
+
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
