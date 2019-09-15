05344634791318854a96e08ff47bb97b96f7a112
Malaber <32635600+Malaber@users.noreply.github.com>
2019-09-15 20:57:46 +0200

    Configure ckeditor for carrierwave with ActiveRecord
    (rails generate ckeditor:install --orm=active_record --backend=carrierwave)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount Ckeditor::Engine => '/ckeditor'
   resources :articles
   get 'privacy', to: 'privacy#get'
   get 'impressum', to: 'impressum#get'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_14_193157) do
+ActiveRecord::Schema.define(version: 2019_09_15_185619) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -48,10 +48,7 @@ ActiveRecord::Schema.define(version: 2019_09_14_193157) do
     t.string "data_file_name", null: false
     t.string "data_content_type"
     t.integer "data_file_size"
-    t.string "data_fingerprint"
     t.string "type", limit: 30
-    t.integer "width"
-    t.integer "height"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["type"], name: "index_ckeditor_assets_on_type"
