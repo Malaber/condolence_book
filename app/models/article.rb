be96003e291d9aeefc460ac2fe6202706d7936b3
Malaber <32635600+Malaber@users.noreply.github.com>
2019-09-16 15:02:44 +0200

    Add methods to go to the next or previous published article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,9 @@
 class Article < ApplicationRecord
+  def next_published
+    self.class.where("id > ? AND published = ?", id, true).first
+  end
+
+  def previous_published
+    self.class.where("id < ? AND published = ?", id, true).last
+  end
 end
